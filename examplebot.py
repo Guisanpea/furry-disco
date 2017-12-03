5319e26402a101102320e35d433e2eccc75d527b
Guisanpea <guisanpea@gmail.com>
2017-12-04 00:07:46 +0100

    Example bot created

---
+++ examplebot.py
@@ -0,0 +1,27 @@
+from telegram.ext import Updater, CommandHandler
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(bot, update):
+    update.message.reply_text('Que pasa pishilla!')
+
+
+def main():
+    updater = Updater(sys.argv[1])
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
